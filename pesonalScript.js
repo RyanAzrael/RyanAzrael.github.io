eb9c18780f2c02bba66582fd0ad2d7598410e724
RyanAzrael <q29241046@gmail.com>
2024-12-06 17:06:41 +0800

    Update 2024/12/06
    Update 2024/12/06

--- pesonalScript.js
+++ pesonalScript.js
@@ -13,11 +13,11 @@ $(function(){
     var con5 = $('.sbt1').offset().top
     var con6 = $('.sbt2').offset().top
     var con7 = $('.sbt3').offset().top
-    var con8 = $('.sw1').offset().top
-    var con9 = $('.sw2').offset().top
-    var con10 = $('.sw3').offset().top
-    var con11 = $('.social').offset().top
 
+    var con8 = $('.social').offset().top
+    var con9 = $('.fbx1').offset().top
+    var con10 = $('.fbx2').offset().top
+    var con11 = $('.fbx3').offset().top
 
     // console.log(el1, el2, el3)
 
@@ -51,45 +51,52 @@ $(function(){
 
         var N = $(window).scrollTop()
 
-        if(N > el2 - 500){
+        if(N > 200){
+            $('.toTop').fadeIn(300)
+        }
+        else{
+            $('.toTop').stop().fadeOut(200)
+        }
+
+        if(N > el2 - 600){
             $('.abMe').animate({opacity:'1'}, 300)
         }
-        if(N > con1 - 500){
+        if(N > con1 - 600){
             $('.abMep').animate({opacity:'1'}, 300)
         }
-        if(N > con2 - 500){
+        if(N > con2 - 600){
             $('.hbt').animate({opacity:'1'}, 300)
         }
-        if(N > con3 - 500){
+        if(N > con3 - 600){
             $('.fa-solid').animate({opacity:'1'}, 300)
         }
-        if(N > con4 - 500){
+        if(N > con4 - 600){
             $('.hbtp').animate({opacity:'1'}, 300)
         }
-        if(N > el3 - 500){
+        if(N > el3 - 600){
             $('.myWork').animate({opacity:'1'}, 300)
         }
-        if(N > con5 - 500){
+        if(N > con5 - 600){
             $('.sbt1').animate({opacity:'1'}, 300)
         }
-        if(N > con6 - 500){
+        if(N > con6 - 600){
             $('.sbt2').animate({opacity:'1'}, 300)
         }
-        if(N > con7 - 500){
+        if(N > con7 - 600){
             $('.sbt3').animate({opacity:'1'}, 300)
-        }
-        if(N > con8 - 400){
-            $('.sw1').animate({opacity:'1'}, 300)
-        }
-        if(N > con9 - 400){
-            $('.sw2').animate({opacity:'1'}, 300)
-        }
-        if(N > con10 - 400){
-            $('.sw3').animate({opacity:'1'}, 300)
-        }
-        if(N > con11 - 850){
+        }       
+        if(N > con8 - 900){
             $('.social').animate({opacity:'1'}, 300)
-       }
+        }
+        if(N > con9 - 600){
+            $('.fbx1').animate({opacity:'1'}, 300)
+        }
+        if(N > con10 - 600){
+            $('.fbx2').animate({opacity:'1'}, 300)
+        }
+        if(N > con11 - 600){
+            $('.fbx3').animate({opacity:'1'}, 300)
+        }
 
 
     })
